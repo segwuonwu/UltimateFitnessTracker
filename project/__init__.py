a1f0907e94b6f6273935ff3ba2124c02064a66b6
segwuonwu <segwuonwu@gmail.com>
2020-04-05 19:30:47 -0700

    working on creating my db

--- project/__init__.py
+++ project/__init__.py
@@ -9,15 +9,22 @@ def create_app():
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/trackerdb'
     app.config['SECRET_KEY'] = 'myultimatefitnesstrackersecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.trackerdb'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/trackerdb'
     
     db.init_app(app)
 
-    # with app.app_context():
-    #     db.create_all()
-
     login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -8,13 +12,40 @@ def login():
         return render_template('login.html')
     if request.method == 'POST':
         pass
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user and not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('auth.login'))
+        
+        login_user(user)
+        return redirect(url_for('main.profile'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
         return render_template('signup.html')
     if request.method == 'POST':
-        pass
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if User:
+            flash('Email address already exists.')
+            return redirect(url_for('auth.signup'))
+
+        new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('main.profile'))
+
 
 @auth.route('/logout')
 def logout():
