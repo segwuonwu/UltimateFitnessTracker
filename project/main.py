4fac9a802796447734e96b407f9bef804f2f0821
segwuonwu <segwuonwu@gmail.com>
2020-04-07 22:42:01 -0700

    Working on my models

--- project/main.py
+++ project/main.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template , jsonify, request, json
 from flask_login import login_required, current_user
+from . import db
+from .models import Exercise, Equipment, Workout
+import requests
 
 main = Blueprint('main', __name__)
 
@@ -11,3 +14,57 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/exercise')
+def exercise():
+    e_list = Exercise.query.all()
+    exercise = []
+    for x in e_list:
+        exercise.append({'name' : x.name, "description": x.description, "muscles":x.muscles, "equipment_id":x.equipment_id})
+    return jsonify({'exercises': exercises})
+    
+@main.route('/exercise/equipment')
+def equipment():
+    euip_list = Equipment.query.all()
+    equipment = []
+    for x in equip_list:
+        equipment.append({'name' : x.name})
+    return jsonify({'equipment': equipment})
+    
+@main.route('/exercise/add_equip', methods=['POST'])
+def add_equipment():
+    data = request.get_json()
+    print(data)
+    new_equipment = Equipment(name=data["name"])
+    db.session.add(new_equipment)
+    db.session.commit()
+
+    return "Done", 201
+
+@main.route('/exercise/add', methods=['POST'])
+def add_exercise():
+    data = request.get_json()
+
+    new_exercise = Exercise(name=data['name'], description=data['description'], muscles=data['muscles'],
+    equipment=data['equipment_id'])
+    db.session.add(new_exercise)
+    db.session.commit()
+
+    return "Done", 201
+
+#@login_required
+@main.route('/workouts')
+def workout():
+    workout_list = Exercise.query.all()
+    workouts = []
+    for x in workout_list:
+        workous.append({'name' : x.name, "date": x.date, "sets": x.sets, "reps": x.reps, "exercerses_id" : x.exercerses_id})
+    return jsonify({'workouts': workouts})
+
+@main.route('/workouts/<edit>', methods=['GET', 'POST'])
+def edit_workout():
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
+
--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from datetime import date
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -12,3 +13,38 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'User(id={self.id}, email="{self.email}", name="{slef.name}", password="{self.password}")'
 
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    muscles = db.Column(db.String(100), nullable=False)
+    equipment_id = db.Column(db.Integer, db.ForeignKey('equipments.id'), nullable=False)
+    equipments = db.relationship('Equipment',
+        backref=db.backref('exercises', lazy=True))
+
+    def __repr__(self):
+        return f'Exercise(id={self.id}, name="{self.name}", description="{self.description}", muscles="{self.muscles}")'
+
+
+class Equipment(db.Model):
+    __tablename__ = 'equipments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'Equipment(id={self.id}, name="{self.name}")'
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    sets = db.Column(db.String(50), nullable=False)
+    reps = db.Column(db.String(50), nullable=False)
+    exercises_id = db.Column(db.Integer, db.ForeignKey(' exercises.id'), nullable=False)
+    exercises = db.relationship('Exercise', lazy='subquery',
+        backref=db.backref('workouts', lazy=True))
+
+    def __repr__(self):
+        return f'Workout(id={self.id}, name="{self.name}", date="{self.date}", sets="{self.sets}", reps="{self.reps}")'
